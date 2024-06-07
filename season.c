9e1678d4ded05a2085598f18003d381093bd02c0
dgued <risc3x@outlook.com>
2024-06-07 23:18:45 +0800

    Modify season.c

--- season.c
+++ season.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# From party night star guy do whole respond.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
