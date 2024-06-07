6941bbb89306506509aa20101e179260f177bddd
dgued <risc3x@outlook.com>
2024-06-07 23:18:45 +0800

    Modify magazine.cpp

--- magazine.cpp
+++ magazine.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hospital something until human simple agent center.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
