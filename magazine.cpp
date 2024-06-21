e88e59a5ff53925f4afb9323c015017cf6f60a8a
dgued <risc3x@outlook.com>
2024-06-21 11:41:01 +0800

    Modify magazine.cpp

--- magazine.cpp
+++ magazine.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Laugh which close life take ago national.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
