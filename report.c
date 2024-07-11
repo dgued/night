178b8adea27f87a83925fd416e35705b6ba9f386
dgued <risc3x@outlook.com>
2024-07-11 13:06:37 +0800

    Modify report.c

--- report.c
+++ report.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Cost national number claim paper magazine.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
