3d5a1b880ed387047ff95a41593f0c8a9f7c8d4e
dgued <risc3x@outlook.com>
2024-08-21 19:55:10 +0800

    Modify image.c

--- image.c
+++ image.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Management nature Mr single evening time.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
