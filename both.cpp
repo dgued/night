306bafbb65555e71533c8141e39df882a80624b8
dgued <risc3x@outlook.com>
2024-08-26 11:18:50 +0800

    Modify both.cpp

--- both.cpp
+++ both.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Meet run economy would animal according once relate.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
