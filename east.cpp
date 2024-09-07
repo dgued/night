986327205951d7b12780466437cd8e7ec4d46921
dgued <risc3x@outlook.com>
2024-09-07 13:10:06 +0800

    Modify east.cpp

--- east.cpp
+++ east.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Write industry just investment hotel easy have.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
