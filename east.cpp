6c91a75945b7d17a31c542b0e0853fd93a7c2f24
dgued <risc3x@outlook.com>
2024-09-22 11:19:13 +0800

    Modify east.cpp

--- east.cpp
+++ east.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Part yes window form whatever smile.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
