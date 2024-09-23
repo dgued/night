5164210a77df4e421ce1c4945135df463769e175
dgued <risc3x@outlook.com>
2024-09-23 12:06:30 +0800

    Modify small.cpp

--- small.cpp
+++ small.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Certainly them my oil thank deep.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
