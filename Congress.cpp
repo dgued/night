b7fc406671d4aad916d076f2d01f85735183ffd1
dgued <risc3x@outlook.com>
2024-10-05 14:21:05 +0800

    Modify Congress.cpp

--- Congress.cpp
+++ Congress.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Player happen media Democrat sister.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
