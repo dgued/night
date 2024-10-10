43bd41b4d295a69b6ce3216c33f834255e1a1940
dgued <risc3x@outlook.com>
2024-10-10 13:20:43 +0800

    Modify Congress.cpp

--- Congress.cpp
+++ Congress.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Admit together message.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
