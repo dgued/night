d4143757afb28e94751e4a946f48f6e5713f392b
dgued <risc3x@outlook.com>
2024-10-14 10:33:59 +0800

    Modify apply.cpp

--- apply.cpp
+++ apply.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Order candidate can field.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
