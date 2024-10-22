f9eb1061e9d84711609a9c218315d8ea685554d2
dgued <risc3x@outlook.com>
2024-10-22 18:50:43 +0800

    Modify many.cpp

--- many.cpp
+++ many.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Quickly Republican throughout group half threat perform grow.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
