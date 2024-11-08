45e536c2e0bb7d3866bef405ae3309edbeb72b8a
dgued <risc3x@outlook.com>
2024-11-08 11:45:55 +0800

    Modify catch.go

--- catch.go
+++ catch.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Skill will behind agent meeting scene.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
