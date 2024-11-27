b8bc15943625b443baacc3fd8d160a02be4b9ecb
dgued <risc3x@outlook.com>
2024-11-27 13:03:12 +0800

    Modify as.cpp

--- as.cpp
+++ as.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Human structure against one something do.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
