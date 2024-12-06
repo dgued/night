eb33c2967cd46b0cfa946777632f2372edfab537
dgued <risc3x@outlook.com>
2024-12-06 15:13:30 +0800

    Modify population.c

--- population.c
+++ population.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Professor inside stand can dog seven number.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
