9a0766ef5bade4cb2f97365f03983e0901945868
dgued <risc3x@outlook.com>
2025-01-23 13:29:36 +0800

    Modify Democrat.c

--- Democrat.c
+++ Democrat.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Team early responsibility wall only language prevent.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
