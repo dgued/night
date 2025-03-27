35ecc90fe0fc7884b237a40a2df1b3e85e95259a
dgued <162856586+dgued@users.noreply.github.com>
2025-03-27 18:39:09 +0800

    feat(core): add population feature

--- population.c
+++ population.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-int main() {
-	printf("Hello, World!\n");
-	return 0;
-}
 
-# Professor inside stand can dog seven number.
-#include <stdio.h>
 int main() {
-	printf("Hello, World!\n");
-	return 0;
+    printf("Hello, World!\n");
+    return 0;
 }
